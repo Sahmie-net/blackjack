bb7753631892f894fd5d2e7e78848a9839456e61
Sahmie-net <sahmie365@gmail.com>
2026-07-04 19:23:14 +0100

    Update UI feedback and fix console logging

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-
-
-
-sum = firstCard + secondCard
-
-
-
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard
+let isAlive = true
+let hasBlackJack = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sumEl")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     if(sum <= 20){
-        message = "Do you want to draw another card?😊"
+        message = "Do you want to draw another card?"
     }else if(sum === 21){
-        message = "You've got Blackjack!🥳"
+        message = "You've got Blackjack!"
+        hasBlackJack = true
     }else{
-        message = "Game over!😭"
+        message = "Game over!"
+        isAlive = false
     }
+    console.log(message)
+    messageEl.textContent = message
 }
