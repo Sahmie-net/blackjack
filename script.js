8dfce4614b49941ed25044eb2a59596b6c6cefc2
Sahmie-net <sahmie365@gmail.com>
2026-07-10 23:02:02 +0100

    Implement for loop to render cards

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let firstCard = 6
-let secondCard = 11
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let isAlive = true
@@ -9,20 +9,33 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sumEl")
 let cardsEl = document.getElementById("cards-el")
 
+function getRandomCard(){
+    let randomNum = Math.floor(Math.random() * 13) + 1
+    if(randomNum === 1){
+        return 11
+    }else if(randomNum > 10){
+        return 10
+    }else{
+        return randomNum
+    }
+}
 function startGame(){
     renderGame()
 }
 
 function renderGame() {
     sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards: "
+    for(i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
     if(sum <= 20){
         message = "Do you want to draw another card?"
     }else if(sum === 21){
         message = "You've got Blackjack!"
         hasBlackJack = true
     }else{
-        message = "Game over!"
+        message = "Game over!"  
         isAlive = false
     }
     console.log(message)
@@ -30,8 +43,8 @@ function renderGame() {
 }
 
 function newCards(){
-    console.log("Drawing new cards from the deck!")
-    let newCard = 3
+    message = "Drawing new cards from the deck!"
+    let newCard = getRandomCard()
     sum += newCard
     cards.push(newCard)
     renderGame()
